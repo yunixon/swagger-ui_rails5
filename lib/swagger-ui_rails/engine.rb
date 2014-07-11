58e12af4b4c8347bdfd24c19e3e9f483626a7dd4
aurelian <oancea@gmail.com>
2014-07-11 18:24:12 +0200

    Re-worked to easly sync with swagger-ui
     * added a rake task to sync up swagger-ui
     * expose stylesheets as separate entities (reset.css could be already included by host app)
     * expose swagger-oauth
     * added a hack to make throbber.gif available to asset pipeline
     * updated swagger-ui to latest version

--- lib/swagger-ui_rails/engine.rb
+++ lib/swagger-ui_rails/engine.rb
@@ -1,4 +1,14 @@
 module Swagger
-  class Engine < Rails::Engine
+  class Engine < ::Rails::Engine
+
+    initializer :assets do |config|
+      Rails.application.config.assets.precompile += %w{ swagger-ui.js }
+      Rails.application.config.assets.precompile += %w{ swagger-oauth.js }
+      Rails.application.config.assets.precompile += %w{ swagger-ui.css }
+      Rails.application.config.assets.precompile += %w{ swagger-ui/reset.css }
+      Rails.application.config.assets.precompile += %w{ swagger-ui/screen.css }
+      Rails.application.config.assets.paths << root.join("app", "assets")
+    end
+
   end
 end
--- swagger-ui_rails.gemspec
+++ swagger-ui_rails.gemspec
@@ -13,9 +13,7 @@ Gem::Specification.new do |gem|
   gem.homepage      = "https://github.com/d4be4st/swagger-ui_rails"
   gem.license       = 'MIT'
 
-  gem.files         = `git ls-files`.split($/)
-  gem.executables   = gem.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
-  gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
+  gem.files = Dir["{app,lib}/**/*", "LICENSE.txt", "Rakefile", "README.md"]
   gem.require_paths = ["lib"]
 
   gem.add_development_dependency "bundler", "~> 1.3"
