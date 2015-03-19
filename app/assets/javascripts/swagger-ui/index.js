d85f3305ffe088a1de75f1b98dec5a417ff57068
Eric Skogen <audionerd@users.noreply.github.com>
2015-03-19 10:50:54 -0500

    sync assets with Swagger-UI v2.1.8-M1

--- app/assets/javascripts/swagger-ui/index.js
+++ app/assets/javascripts/swagger-ui/index.js
@@ -3,11 +3,9 @@
 //= require ./lib/jquery.slideto.min.js
 //= require ./lib/jquery.wiggle.min.js
 //= require ./lib/jquery.ba-bbq.min.js
-//= require ./lib/handlebars-1.0.0.js
+//= require ./lib/handlebars-2.0.0.js
 //= require ./lib/underscore-min.js
 //= require ./lib/backbone-min.js
-//= require ./lib/swagger.js
 //= require ./lib/swagger-client.js
 //= require ./swagger-ui.js
 //= require ./lib/highlight.7.3.pack.js
-
